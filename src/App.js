04f59980e130c8125f0d6ab018c0c7ff3cd5acb7
MODIFYC <chsj5742@naver.com>
2022-03-09 21:42:12 +0900

    beginReact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import AudioSample from './AudioSample';
+import UserList from './UserList';
 
 function App() {
   //name 정의
@@ -17,6 +18,31 @@ function App() {
     fontSize: 24, // 기본 단위 px
     padding: '1rem' // 다른 단위 사용 시 문자열로 설정
   }
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+    nextId.current += 1;
+  };
+  
 
   return (
     <>
@@ -33,6 +59,8 @@ function App() {
       <InputSample />
       <hr />
       <AudioSample />
+      <hr />
+      <UserList users={users} />
     </>
   );
 }
