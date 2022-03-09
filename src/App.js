690d9bac7b8ccd4605f724931e991e63fa84cd1a
MODIFYC <chsj5742@naver.com>
2022-03-09 21:53:45 +0900

    beginReact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import AudioSample from './AudioSample';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
   //name 정의
@@ -18,7 +19,23 @@ function App() {
     fontSize: 24, // 기본 단위 px
     padding: '1rem' // 다른 단위 사용 시 문자열로 설정
   }
-  const users = [
+
+  // user create
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  // user list
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -34,12 +51,23 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
 
+  // user set
   const nextId = useRef(4);
   const onCreate = () => {
-    // 나중에 구현 할 배열에 항목 추가하는 로직
-    // ...
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+    // setUsers(users.concat(user));
+
+    setInputs({
+      username: '',
+      email: ''
+    });
     nextId.current += 1;
   };
   
@@ -60,12 +88,15 @@ function App() {
       <hr />
       <AudioSample />
       <hr />
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
       <UserList users={users} />
     </>
   );
 }
 
 export default App;
-
-// props는 부모컴포넌트에서 자식컴포넌트에게 전달. 
-// 프레임만 같고 콘텐츠는 다르게
